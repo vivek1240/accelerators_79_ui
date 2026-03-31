240d34a7e14d85fa9721799132598e67bd64e56d
Harsh Agrawal <harshagrawal@Harshs-MacBook-Air.local>
2026-03-31 18:23:05 +0530

    added bypass for login as admin

--- mvp_demo_backend/src/models/User.js
+++ mvp_demo_backend/src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, lowercase: true },
     hashed_password: { type: String, required: true },
     name: { type: String, default: null },
-    role: { type: String, default: 'user' },
+    role: { type: String, default: 'admin' },
     is_allowed: { type: Boolean, default: false },
   },
   { timestamps: true }
